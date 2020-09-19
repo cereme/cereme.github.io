1740ec78973e4b0edf984862fbf7f8e500dade66
cereme(Sang Yun Pae) <bsy@breadlab.net>
2020-09-19 16:41:45 +0900

    Make ThemeSwitch invisible

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -14,6 +14,7 @@ export const Layout = ({ location, title, children }) => {
   return (
     <React.Fragment>
       <Top title={title} location={location} rootPath={rootPath} />
+      <div style={{ display: "none" }}><ThemeSwitch /></div>
       <div
         style={{
           marginLeft: `auto`,
