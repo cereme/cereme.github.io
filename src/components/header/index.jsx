88c6bdfb1f2fbac9452c26a29e3a6edd56b5cdb2
cereme(Sang Yun Pae) <bsy@breadlab.net>
2020-09-19 19:01:48 +0900

    Make ThemeSwitch available

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { ThemeSwitch } from './../theme-switch'
 
 import './index.scss'
 
@@ -12,10 +13,12 @@ export const Header = ({ title, location, rootPath, shortDescription }) => {
           <Link to={`/`} className="link">
             {title}
           </Link>
+          <div className="mobile-theme-switch"><ThemeSwitch /></div>
         </h1>
         <Link to={`/about`} className="shortDescription-header link">
           {shortDescription}
         </Link>
+        <div className="theme-switch"><ThemeSwitch /></div>
       </div>
 
     )
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { Top } from '../components/top'
 import { Header } from '../components/header'
-import { ThemeSwitch } from '../components/theme-switch'
 import { Footer } from '../components/footer'
 import { rhythm } from '../utils/typography'
 
@@ -17,7 +16,6 @@ export const Layout = ({ location, title, shortDescription, children }) => {
   return (
     <React.Fragment>
       <Top title={title} location={location} rootPath={rootPath} />
-      <div style={{ display: "none" }}><ThemeSwitch /></div>
       <div
         style={{
           marginLeft: `auto`,
